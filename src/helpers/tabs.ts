0cbc5c3c44da2b44388ac345d5fafefe16d2ada5
Veljko Skarich <veljko.skarich@gmail.com>
2024-03-30 16:36:42 -0600

    Testing git token.

--- src/helpers/tabs.ts
+++ src/helpers/tabs.ts
@@ -3,5 +3,6 @@ import { tabs } from 'webextension-polyfill'
 export async function getCurrentTab() {
   const list = await tabs.query({ active: true, currentWindow: true })
 
+  
   return list[0]
 }
